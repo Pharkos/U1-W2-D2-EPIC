a6a1502b75ba66e42c20331c2dd56eae9ea95ef0
Pharkos <vakhopharkosadze1@gmail.com>
2025-07-30 22:55:51 +0200

    Update 2

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -46,6 +46,19 @@ if (num19 >= 8 || num19 - num20 === 8 || num19 + num20 === 8 || num20 >= 8) {
   Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
 */
 
+let totalShoppingCart = 49;
+const costoDiSpedizione = 10;
+let carello = 0;
+let conto = 0;
+
+if (carello + totalShoppingCart < 50) {
+  conto = costoDiSpedizione + totalShoppingCart;
+
+  console.log("la cifra da pagare + spedizione " + conto);
+} else {
+  console.log("spedizione gratuita  " + totalShoppingCart);
+}
+
 /* ESERCIZIO 6
   Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
   Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
@@ -56,6 +69,13 @@ if (num19 >= 8 || num19 - num20 === 8 || num19 + num20 === 8 || num20 >= 8) {
   Utilizzando un blocco condizionale, crea un algoritmo per ordinarle secondo il loro valore, dal più alto al più basso.
   Alla fine mostra il risultato in console.
 */
+
+const var1 = 77;
+const var2 = 109;
+const var3 = 780;
+
+if ((var1 > var2 && var3) || (var2 > var1 && var3) || (var3 > var1 && var2)) {
+}
 
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
@@ -71,21 +91,25 @@ if (typeof year === "number") {
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let ourNumber = 779;
+
+if (ourNumber % 2 === 0) {
+  console.log("il numero fornito e' pari ");
+} else {
+  console.log("il numero fornito e' dispari");
+}
 
 /* ESERCIZIO 10
   Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
-  let val = 7
-  if (val < 10) {
-      console.log("Meno di 10");
-    } else if (val < 5) {
-      console.log("Meno di 5");
-    } else {
-      console.log("Uguale a 10 o maggiore");
-    }
-*/
-
-/* SCRIVI QUI LA TUA RISPOSTA */
+ */
+let val = 7;
+if (val < 10) {
+  console.log("Meno di 10");
+} else if (val < 5) {
+  console.log("Meno di 5");
+} else {
+  console.log("Uguale a 10 o maggiore");
+}
 
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
@@ -97,19 +121,25 @@ const me = {
   skills: ["javascript", "html", "css"],
 };
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+me["city"] = "toronto";
+
+console.log(me);
 
 /* ESERCIZIO 12
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+delete me.lastName;
+
+console.log(me);
 
 /* ESERCIZIO 13
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+me.skills.pop();
+
+console.log(me);
 
 /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
