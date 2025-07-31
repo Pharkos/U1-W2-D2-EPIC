2259f703a3ad6cb76d06cc8e9f84679d2dd77131
Pharkos <vakhopharkosadze1@gmail.com>
2025-07-31 09:48:43 +0200

    update 3

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -145,10 +145,14 @@ console.log(me);
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let myArrey = [];
+
+for (let i = 0; i <= 10; i++) {
+  myArrey.push(i);
+
+  console.log(myArrey);
+}
 
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
-
-/* SCRIVI QUI LA TUA RISPOSTA */
